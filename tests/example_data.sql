7b3a50cdce050ef0e0b757dc0658e236517bdbea
Erik Teichmann <kontakt.teichmann@gmail.com>
2018-11-20 17:51:29 +0100

    Fix: Wrong column name in test data

--- tests/example_data.sql
+++ tests/example_data.sql
@@ -4,7 +4,7 @@ INSERT INTO payment_method (name) VALUES ("Cash"), ("Credit Card");
 
 INSERT INTO category (name) VALUES ("Fun"), ("Work");
 
-INSERT INTO money_transfer (transfer_date, amount, contact_id,
+INSERT INTO money_transfer (transaction_date, amount, contact_id,
                             payment_method_id, category_id)
 VALUES
   (1542727811, 50.00, 1, 1, 1),
